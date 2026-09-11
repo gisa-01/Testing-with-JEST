27eafc30b67b1eec9ae31e86911488127016ba0a
Gisa <gisaaimesavio@gmail.com>
2026-09-11 12:42:09 +0200

    Testing strings

--- src/math.test.ts
+++ src/math.test.ts
@@ -1,3 +1,11 @@
-test('not equal', () => {
-  expect('hello').not.toBe('world');
-})
+describe('String functions', () => {
+  test('contain', () => {
+    expect('Typescript is powerful',).toContain('Typescript');
+  });
+  test('length', () => {
+    expect('jest').toHaveLength(4);
+  });
+  test('array contains', () => {
+    expect(["React", "Next.js", "React Native"]).toContain('React Native');
+  });
+});

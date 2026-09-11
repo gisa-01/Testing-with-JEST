facb1b15b981579fd2abb57ae0df6669279bb608
Gisa <gisaaimesavio@gmail.com>
2026-09-11 12:15:18 +0200

    use of describe ()

--- src/math.test.ts
+++ src/math.test.ts
@@ -1,23 +1,15 @@
-test("checks an object", () => {
-  const user = {
-    name: "Gisa",
-    age: 20,
-  };
+import { add } from './math';
 
-  expect(user).toBe({
-    name: "Gisa",
-    age: 20,
+describe('Math functions', () => {
+  test('add two numbers', () => {
+    expect(add(2,3)).toBe(5);
   });
-});
 
-test("checks an object", () => {
-  const user = {
-    name: "Gisa",
-    age: 20,
-  };
+   test("checks that 10 is greater than 5", () => {
+    expect(10).toBeGreaterThan(5);
+  });
 
-  expect(user).toEqual({
-    name: "Gisa",
-    age: 20,
+  test("checks that 20 is less than 30", () => {
+    expect(20).toBeLessThan(30);
   });
 });

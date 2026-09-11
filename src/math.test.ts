6b03514da3be69d90903a069de499c70b6a18929
Gisa <gisaaimesavio@gmail.com>
2026-09-11 23:44:49 +0200

    mocking

--- src/math.test.ts
+++ src/math.test.ts
@@ -1,25 +1,11 @@
-import { describe, test, expect } from "@jest/globals";
+const greet = jest.fn();
 
-const user = {
-  name: "Gisa",
-  role: "Developer",
-  skills: ["React", "TypeScript", "Jest"],
-};
+greet.mockReturnValue("Hello Gisa");
 
-describe("User", () => {
-  describe("Profile", () => {
-    test("name is Gisa", () => {
-      expect(user).toHaveProperty("name", "Gisa");
-    });
+test("mock function returns a value", () => {
+  expect(greet()).toBe("Hello Gisa");
+});
 
-    test("role is Developer", () => {
-      expect(user).toHaveProperty("role", "Developer");
-    });
-  });
-
-  describe("Skills", () => {
-    test("contains TypeScript", () => {
-      expect(user.skills).toContain("TypeScript");
-    });
-  });
+test("mock function was called once", () => {
+  expect(greet).toHaveBeenCalledTimes(1);
 });

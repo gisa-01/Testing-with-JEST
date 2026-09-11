28af0fa5eeabc64caf34b6fd3190268fe0546b15
Gisa <gisaaimesavio@gmail.com>
2026-09-11 11:52:21 +0200

    toEqual vs toBe

--- src/math.test.ts
+++ src/math.test.ts
@@ -1,3 +1,23 @@
-test('checking large number', () => {
-  expect('jest').toBe('jest');
-})
+test("checks an object", () => {
+  const user = {
+    name: "Gisa",
+    age: 20,
+  };
+
+  expect(user).toBe({
+    name: "Gisa",
+    age: 20,
+  });
+});
+
+test("checks an object", () => {
+  const user = {
+    name: "Gisa",
+    age: 20,
+  };
+
+  expect(user).toEqual({
+    name: "Gisa",
+    age: 20,
+  });
+});

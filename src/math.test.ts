62fa64393ef4de50bb1ec38494147c37ea463a51
Gisa <gisaaimesavio@gmail.com>
2026-09-11 12:26:43 +0200

    use of not

--- src/math.test.ts
+++ src/math.test.ts
@@ -1,15 +1,3 @@
-import { add } from './math';
-
-describe('Math functions', () => {
-  test('add two numbers', () => {
-    expect(add(2,3)).toBe(5);
-  });
-
-   test("checks that 10 is greater than 5", () => {
-    expect(10).toBeGreaterThan(5);
-  });
-
-  test("checks that 20 is less than 30", () => {
-    expect(20).toBeLessThan(30);
-  });
-});
+test('not equal', () => {
+  expect('hello').not.toBe('world');
+})
